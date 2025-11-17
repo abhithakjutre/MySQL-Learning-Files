97c92e4986c8a194a3751a5f4666d47480e959df
Abhishek Thakur <144336328+abhithakjutre@users.noreply.github.com>
2025-11-17 09:04:29 +0530

    This is updated code

---
+++ 2. ALTER_RENAME_MODIFY.sql
@@ -0,0 +1,25 @@
+-- CREATE DATABASE mysql_learn; 
+USE mysql_learn;
+
+-- CREATE TABLE programmer( 
+-- id INT AUTO_INCREMENT PRIMARY KEY,
+-- department ENUM("SALES", "SUPPORT", "IT"), 
+-- user_password VARCHAR(100) NOT NULL,
+-- created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+
+
+-- )
+
+
+
+
+-- ALTER TABLE programmer ADD COLUMN username VARCHAR(100) NOT NULL;
+-- ALTER TABLE programmer DROP COLUMN username; 
+
+-- ALTER TABLE programmer MODIFY COLUMN username VARCHAR(100) AFTER id;
+SELECT * FROM programmer;
+
+
+
+
+
